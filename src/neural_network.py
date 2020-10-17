ca3720c43515123fcee9097e4cbadd2924c83c84
SL8R <ignacio.slater@ug.uchile.cl>
2020-10-17 18:52:52 -0300

    Corrections
    Took 44 minutes

--- src/neural_network.py
+++ src/neural_network.py
@@ -1,115 +1,106 @@
 from typing import Callable, List, Union
 
 import torch
-from torch import Tensor, nn
-from torch.nn import Module, Parameter, ParameterList
+from torch import Tensor
+from torch.nn import Module
 
-from activation_functions import relu, sig, softmax
+from activation_functions import celu, relu, softmax, swish
 
 ActivationFunction = Union[Callable[[torch.Tensor, float], Tensor], Callable[[Tensor], Tensor]]
 
 
 class FFNN(Module):
-    """ Implementation of a Feed Forward Neural Network (FFNN).
+    """ Implementation of a Feed Forward Neural Network (FFNN)."""
+    __computed_layers: [Tensor]
 
-        :param size_in:
-            the number of neurons in the input layer.
-        :param hidden_sizes:
-            the list of sizes of the hidden layers.
-        :param activation_functions:
-            the list of the activation functions.
-        :param size_out:
-            the number of neurons on the output layer.
-    """
+    def __init__(self, F: int, l_h: List[int], l_a: List[ActivationFunction], C: int,
+                 l_a_params=None):
+        """
+        Creates a new feed forward neural network.
 
-    def __init__(self, size_in: int, hidden_sizes: List[int],
-                 activation_functions: List[ActivationFunction],
-                 function_parameters: List,
-                 size_out: int):
-        assert size_out >= 2
+        Args:
+            F:
+                the number of neurons in the input layer.
+            l_h:
+                the list of sizes of the hidden layers.
+            l_a:
+                the list of the activation functions.
+            C:
+                the number of neurons on the output layer.
+            l_a_params:
+                the parameters for the activation functions
+        """
         super(FFNN, self).__init__()
-        neurons = [size_in]
-        neurons.extend(hidden_sizes)
-        neurons.append(size_out)
-        self.__size = len(activation_functions)
-        self.__weights = ParameterList(
-            [Parameter(torch.rand(neurons[i], neurons[i + 1])) for i in range(0, len(neurons) - 1)])
-        self.__biases = ParameterList(
-            [Parameter(torch.zeros(layer_size)) for layer_size in hidden_sizes])
-        self.__activation_functions = activation_functions
-        self.__function_parameters = function_parameters
+
+        sizes = [F] + l_h + [C]
+        self.__weights = torch.nn.ParameterList(
+            [torch.nn.Parameter(torch.randn(sizes[i], sizes[i + 1])) for i in
+             range(len(sizes) - 1)])
+        self.__biases = torch.nn.ParameterList(
+            [torch.nn.Parameter(torch.zeros(h)) for h in sizes[1:]])
+        self.__activation_functions = l_a
+        self.__function_parameters = torch.nn.ParameterList(
+            [torch.nn.Parameter(torch.tensor(p)) if p else None for p in
+             l_a_params]) if l_a_params is not None else [None] * len(l_a)
+        self.__computed_layers = []
 
     def forward(self, nn_input: Tensor) -> Tensor:
+        """
+        Performs a forward feeding to the network.
+
+        Args:
+            nn_input:
+                the input of the network.
+        Returns:
+            the output of the feeding normalized with the softmax function.
+        """
         out = nn_input
+        self.__computed_layers = [nn_input]
         for weights, biases, activation, params in zip(self.__weights[:-1], self.__biases[:-1],
                                                        self.__activation_functions,
                                                        self.__function_parameters):
             out = activation(torch.mm(out, weights) + biases) if params is None else activation(
                 torch.mm(out, weights) + biases, params.item())
+            self.__computed_layers.append(out)
         return softmax(torch.mm(out, self.__weights[-1]) + self.__biases[-1], dim=1)
 
     def backward(self, x, y, y_pred):
         """"""
         it = self.__size - 1
         batch_size = x.size()[0]
-        out_layer_loss = (1 / batch_size) * (y_pred - y)
+        out_layer_loss = (y_pred - y) / batch_size
+        out: Tensor = self.__weights[-1]
+        out.grad = self.__activation_functions[-1]
         # TODO
 
     # region : Utility
-    def print_weights(self) -> None:
+    def summary(self) -> None:
         """
         Prints a summary of the weights of this network.
         """
-        print("Weights:")
-        for layer, weight in enumerate(self.weights):
-            print(f"\tLayer {layer}: {weight.size()}")
-
-    def print_biases(self) -> None:
-        """
-        Prints a summary of the biases of this network.
-        """
-        print("Biases:")
-        for layer, bias in enumerate(self.biases):
-            print(f"\tLayer {layer}: {bias.size()}")
-
-    def print_activation_functions(self):
-        """
-        Prints a summary of the biases of this network.
-        """
-        print("Activation functions:")
-        for layer, fun in enumerate(self.activation_functions):
-            print(f"\tLayer {layer}: {fun.__name__}")
+        for name, p in self.named_parameters():
+            print(f'{name}:\t{p.size()}')
 
     # endregion
 
     # region : Properties
+    def load_weights(self, weights: Tensor, outputs: Tensor, biases: Tensor, classes: Tensor):
+        self.__weights = torch.nn.ParameterList(
+            [torch.nn.Parameter(W) for W in weights + [outputs]])
+        self.__biases = torch.nn.ParameterList([torch.nn.Parameter(b) for b in biases + [classes]])
+
     @property
     def weights(self):
         return self.__weights.parameters()
 
-    @weights.setter
-    def weights(self, nn_weights: List[Tensor]):
-        self.__weights = ParameterList(
-            [nn.Parameter(layer_weights) for layer_weights in nn_weights])
-
     @property
-    def biases(self):
-        return self.__biases.parameters()
-
-    @biases.setter
-    def biases(self, nn_biases: List[Tensor]):
-        self.__biases = ParameterList([Parameter(layer_biases) for layer_biases in nn_biases])
-
-    @property
-    def activation_functions(self):
-        return self.__activation_functions
-
-    @activation_functions.setter
-    def activation_functions(self, nn_functions: List[Tensor]):
-        self.__activation_functions = nn_functions
+    def in_size(self):
+        return self.__weights[0].shape[0]
     # endregion
 
 
 if __name__ == '__main__':
-    network = FFNN(300, [50, 30], [relu, sig], 10)
-    print(network.forward(torch.randn(1, 300)))
+    model = FFNN(300, [50, 30, 25, 20], [relu, celu, swish, relu], 10, [None, .5, .5, None])
+    print(model.summary())
+    print(model.in_size)
+    print(model(torch.rand(2, 300)))
