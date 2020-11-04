f6a0b6d1ed1758a82162c5411df3d7bd19f10e7b
SL8R <ignacio.slater@ug.uchile.cl>
2020-11-04 16:57:57 -0300

    Snapshot
    Took 1 minute

--- src/gradient_descent.py
+++ src/gradient_descent.py
@@ -1,3 +1,10 @@
+"""
+"Back to training" (c) by Ignacio Slater M.
+"Back to training" is licensed under a
+Creative Commons Attribution 4.0 International License.
+You should have received a copy of the license along with this
+work. If not, see <http://creativecommons.org/licenses/by/4.0/>.
+"""
 from typing import Tuple
 
 import torch
